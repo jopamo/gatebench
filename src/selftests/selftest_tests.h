effd44f8e15b4f062350221ca400a11f50e280d1
Paul Moses <p@1g4.org>
2026-01-31 07:25:17 -0600

    src/selftests/test_invalid_entry_attrs.c: add test for invalid gate entry attribute sizes
    Missing test coverage for kernel validation of TCA_GATE_ENTRY_*
    attribute lengths.
    
    Adds new selftest sending netlink messages with undersized INTERVAL,
    IPV, and MAX_OCTETS attributes in TCA_GATE_ONE_ENTRY nests, verifies
    -EINVAL rejection, integrates into build and test runner.

--- src/selftests/selftest_tests.h
+++ src/selftests/selftest_tests.h
@@ -22,6 +22,7 @@ int gb_selftest_multiple_entries(struct gb_nl_sock* sock, uint32_t base_index);
 int gb_selftest_malformed_nesting(struct gb_nl_sock* sock, uint32_t base_index);
 int gb_selftest_replace_append_entries(struct gb_nl_sock* sock, uint32_t base_index);
 int gb_selftest_invalid_action_control(struct gb_nl_sock* sock, uint32_t base_index);
+int gb_selftest_invalid_entry_attrs(struct gb_nl_sock* sock, uint32_t base_index);
 int gb_selftest_bad_attribute_size(struct gb_nl_sock* sock, uint32_t base_index);
 int gb_selftest_param_validation(struct gb_nl_sock* sock, uint32_t base_index);
 int gb_selftest_replace_invalid(struct gb_nl_sock* sock, uint32_t base_index);
